8f6423c79e97da2c23ef430ea0c369b49cff9cfb
leonardo valle <leonardo.valle@eduzz.com>
2023-04-24 10:06:04 -0300

    feature: inicia componente de venda

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,17 +1,15 @@
 import { Header } from "../Header";
-
 interface IProps {
   children: React.ReactNode;
 }
 
 export const Layout: React.FC<IProps> = ({ children }) => {
   return (
-    <div className="w-screen h-screen flex bg-slate-500">
+    <div className="w-screen h-screen flex bg-slate-500 ">
       <header className="border border-black w-1/6 h-full">
         <Header />
       </header>
-
-      <main className="bg-red-400 flex-1">{children}</main>
+      {children}
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import { Layout } from "@/components/Layout";
+import { Sale } from "@/components/Sale";
 import { Inter } from "next/font/google";
 
 export default function Home() {
-  return <Layout>vendas</Layout>;
+  return (
+    <Layout>
+      <Sale />
+    </Layout>
+  );
 }
